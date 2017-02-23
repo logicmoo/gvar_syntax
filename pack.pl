8725ebef040c4221fab91eea9e0b72db442157aa
root <logicmoo@gmail.com>
2017-02-23 07:53:43 -0800

    v0.0.7

--- pack.pl
+++ pack.pl
@@ -2,9 +2,9 @@ name(gvar_syntax).
 title('Global Variable Syntax').
 
 version('0.0.7').
-download('https://github.com/TeamSPoon/pack_gvar_syntax/releases/*.zip').
+download('https://github.com/TeamSPoon/gvar_syntax/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 maintainer( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
-home('https://github.com/TeamSPoon/pack_gvar_syntax').
+home('https://github.com/TeamSPoon/gvar_syntax').
