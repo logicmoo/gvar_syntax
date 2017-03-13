86836478d7a58181571b8fd7b7d5fa482e39a3b7
root <logicmoo@gmail.com>
2017-03-13 06:53:48 -0700

    v1.1.113

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(gvar_syntax).
 title('Global Variable Syntax').
 
-version('1.1.112').
+version('1.1.113').
 download('https://github.com/TeamSPoon/gvar_syntax/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
